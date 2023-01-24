4beee55406ae663c0e620969a0ec46b3084f4d23
mbermudezv <mauriciobermudez@hotmail.com>
2023-01-24 09:02:19 -0600

    Camara inicial proceso

--- app/src/main/java/com/example/controlapp/MainActivity.kt
+++ app/src/main/java/com/example/controlapp/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.controlapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AddAPhoto
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AlignYourBodyElement()
+        }
     }
+}
+
+// Step: Align your body - Alignment
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+    Column(
+        modifier = modifier
+    ) {
+        Icon(
+            Icons.Rounded.AddAPhoto,
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.boton_camara)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAlignYourBodyElement() {
+    AlignYourBodyElement()
 }
