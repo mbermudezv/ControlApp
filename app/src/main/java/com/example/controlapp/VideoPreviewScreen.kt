1cf8cf359945f061a82aaabb65e63c1b9170ad69
mbermudezv <mauriciobermudez@hotmail.com>
2023-03-03 20:54:07 -0600

    Boton Guardar

--- app/src/main/java/com/example/controlapp/VideoPreviewScreen.kt
+++ app/src/main/java/com/example/controlapp/VideoPreviewScreen.kt
@@ -1,12 +1,18 @@
 package com.example.controlapp
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -37,10 +43,36 @@ fun VideoPreviewScreen(
                 },
                 modifier = Modifier.fillMaxSize()
             )
+            VideoPreviewScreen()
         }
     ) {
         onDispose {
             exoPlayer.release()
         }
     }
+}
+
+@Composable
+fun VideoPreviewScreen() {
+    Row()
+    {
+        Column {
+            IconButton(
+                onClick = {
+                    Log.d("YOUR TAG", "Hey You Just Clicked Me!");
+                },
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.listo),
+                    contentDescription = "",
+                    modifier = Modifier.size(64.dp)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Guardar")
+            }
+        }
+    }
+
 }
