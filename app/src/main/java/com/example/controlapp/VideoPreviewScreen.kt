b4f0ba1f27fa5f7d751e1a3a9d2529552cf154ab
mbermudezv <mauriciobermudez@hotmail.com>
2023-03-17 22:14:08 -0600

    Boton Guardar Extendido

--- app/src/main/java/com/example/controlapp/VideoPreviewScreen.kt
+++ app/src/main/java/com/example/controlapp/VideoPreviewScreen.kt
@@ -1,15 +1,20 @@
 package com.example.controlapp
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -19,19 +24,20 @@ import com.google.android.exoplayer2.ui.StyledPlayerView
 fun VideoPreviewScreen(
     uri: String
 ) {
-    val context = LocalContext.current
 
-    val exoPlayer = remember(context) {
-        ExoPlayer.Builder(context).build().apply {
-            setMediaItem(MediaItem.fromUri(uri))
-            prepare()
+    Box(modifier = Modifier.fillMaxSize()){
+
+        val context = LocalContext.current
+
+        val exoPlayer = remember(context) {
+            ExoPlayer.Builder(context).build().apply {
+                setMediaItem(MediaItem.fromUri(uri))
+                prepare()
+            }
         }
-    }
 
-    DisposableEffect(
+        DisposableEffect(
 
-        Column
-        {
             AndroidView(
                 factory = { context ->
 
@@ -43,17 +49,29 @@ fun VideoPreviewScreen(
                 modifier = Modifier.padding(1.dp)
             )
 
-            // Compose Button
-            androidx.compose.material.Button(onClick = {Log.d("Guardar","Guardar") }) {
-                Text("Guardar")
+        ) {
+            onDispose {
+                exoPlayer.release()
             }
-
         }
 
-    ) {
-        onDispose {
-            exoPlayer.release()
-        }
+        ExtendedFloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(12.dp),
+            text = {
+                Text("GUARDAR")
+            },
+            shape = RoundedCornerShape(
+                ZeroCornerSize
+            ),
+            backgroundColor = Color(0xFF83838A),
+            contentColor = Color(0xFF041631),
+            onClick = { Log.d("Guardar","Guardar") }
+        )
+
     }
+
+
 }
 
